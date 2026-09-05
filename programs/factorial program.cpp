f13f3f5440d4d0ab0a204c8e0351bddd6366a003
Gaurav_Patil <162122864+Gaurav1523@users.noreply.github.com>
2026-09-05 08:48:18 +0530

    Add files via upload

---
+++ programs/factorial program.cpp
@@ -0,0 +1,17 @@
+#inclde<stdio.h>
+int main()
+{
+//	int a,b;
+//	printf("enter any number");
+//	scanf("%d\n",&a);
+//	
+//	if(a>0)
+//	{
+//		printf("factorial of a given no does not exist");
+//	}
+//	else
+//	{
+//		
+//	}
+
+}
